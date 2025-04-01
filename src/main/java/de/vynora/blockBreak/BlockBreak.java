c6692ed9b9139bfe94c5631fac8f874ff4e3cf1c
ShadowStrikeTwitch <leonsahl@t-online.de>
2025-04-01 15:14:38 +0200

    Neue Version

--- src/main/java/de/vynora/blockBreak/BlockBreak.java
+++ src/main/java/de/vynora/blockBreak/BlockBreak.java
@@ -3,10 +3,13 @@ package de.vynora.blockBreak;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
@@ -19,8 +22,8 @@ public class BlockBreak extends JavaPlugin implements CommandExecutor {
 
     private final List<Player> players = new ArrayList<>();
     private final int towerHeight = 20;
-    private final int towerSpacing = 5;
-    private final Material[] blockTypes = {Material.STONE, Material.OAK_LOG, Material.DIRT};
+    private final int towerSpacing = 10; // Abstand zwischen den Türmen
+    private final Material[] blockTypes = {Material.STONE, Material.DIRT, Material.OAK_LOG};
 
     @Override
     public void onEnable() {
@@ -58,11 +61,16 @@ public class BlockBreak extends JavaPlugin implements CommandExecutor {
         World world = players.get(0).getWorld();
         Random random = new Random();
 
-        for (int i = 0; i < players.size(); i++) {
+        // Erstellen der 4 Türme in einem Viereck
+        int[] xOffsets = {-towerSpacing, 0, towerSpacing, 0};
+        int[] zOffsets = {0, towerSpacing, 0, -towerSpacing};
+
+        for (int i = 0; i < 4; i++) {
+            int x = xOffsets[i];
+            int z = zOffsets[i];
             Player player = players.get(i);
-            int x = i * towerSpacing;
-            int y = world.getHighestBlockYAt(x, 0) + towerHeight;
-            int z = 0;
+
+            int y = world.getHighestBlockYAt(x, z) + towerHeight;
 
             // Erstelle den Turm aus Goldblöcken
             for (int j = 0; j < towerHeight; j++) {
@@ -82,27 +90,44 @@ public class BlockBreak extends JavaPlugin implements CommandExecutor {
                 }
             }
 
-            // Teleportiere den Spieler auf seinen Turm
+            // Teleportiere den Spieler auf einen der Türme
             player.teleport(new org.bukkit.Location(world, x + 0.5, y + 1, z + 0.5));
+
+            // Gib dem Spieler die benötigten Werkzeuge
+            giveToolsToPlayer(player);
+
+            // Nachricht, dass das Spiel in 5 Sekunden startet
             player.sendMessage("Das Spiel startet in 5 Sekunden!");
         }
 
-        // Countdown von 5 Sekunden vor Spielstart
+        // Verzögerung von 5 Sekunden vor Spielstart
         new BukkitRunnable() {
-            int countdown = 5;
-
             @Override
             public void run() {
-                if (countdown > 0) {
-                    Bukkit.broadcastMessage("Spiel startet in " + countdown + " Sekunden!");
-                    countdown--;
-                } else {
-                    Bukkit.broadcastMessage("Los geht's!");
-                    startGameLoop();
-                    this.cancel();
-                }
+                startGameLoop();
             }
-        }.runTaskTimer(this, 0, 20); // 20 Ticks = 1 Sekunde
+        }.runTaskLater(this, 100); // 100 Ticks = 5 Sekunden
+    }
+
+    private void giveToolsToPlayer(Player player) {
+        // Erstelle die benötigten Werkzeuge für den Spieler
+        ItemStack pickaxe = new ItemStack(Material.DIAMOND_PICKAXE);
+        ItemMeta pickaxeMeta = pickaxe.getItemMeta();
+        pickaxeMeta.setDisplayName("Spitzhacke");
+        pickaxe.setItemMeta(pickaxeMeta);
+
+        ItemStack shovel = new ItemStack(Material.DIAMOND_SHOVEL);
+        ItemMeta shovelMeta = shovel.getItemMeta();
+        shovelMeta.setDisplayName("Schaufel");
+        shovel.setItemMeta(shovelMeta);
+
+        ItemStack axe = new ItemStack(Material.DIAMOND_AXE);
+        ItemMeta axeMeta = axe.getItemMeta();
+        axeMeta.setDisplayName("Axt");
+        axe.setItemMeta(axeMeta);
+
+        // Gib dem Spieler die Werkzeuge
+        player.getInventory().addItem(pickaxe, shovel, axe);
     }
 
     private void startGameLoop() {
@@ -116,10 +141,19 @@ public class BlockBreak extends JavaPlugin implements CommandExecutor {
                     int y = player.getLocation().getBlockY();
                     int z = player.getLocation().getBlockZ();
 
-                    if (player.getWorld().getBlockAt(x, y - 1, z).getType() == Material.GREEN_WOOL) {
+                    Block blockBelow = player.getWorld().getBlockAt(x, y - 1, z);
+
+                    // Wenn der Spieler den grünen Wollblock erreicht hat
+                    if (blockBelow.getType() == Material.GREEN_WOOL) {
                         Bukkit.broadcastMessage(player.getName() + " hat gewonnen!");
                         this.cancel();
                         return;
+                    }
+
+                    // Überprüfe, ob der Spieler das richtige Werkzeug für den Block hat
+                    if (!isCorrectToolForBlock(player, blockBelow)) {
+                        player.sendMessage("Du hast das falsche Werkzeug!");
+                        continue;
                     }
 
                     // Ändere die zwei Blöcke unter dem Spieler
@@ -129,6 +163,22 @@ public class BlockBreak extends JavaPlugin implements CommandExecutor {
                     }
                 }
             }
-        }.runTaskTimer(this, 20, 20);
+        }.runTaskTimer(this, 20, 20); // Alle 20 Ticks = 1 Sekunde
+    }
+
+    private boolean isCorrectToolForBlock(Player player, Block block) {
+        Material blockType = block.getType();
+        Material toolType = player.getInventory().getItemInMainHand().getType();
+
+        switch (blockType) {
+            case STONE:
+                return toolType == Material.DIAMOND_PICKAXE;
+            case DIRT:
+                return toolType == Material.DIAMOND_SHOVEL;
+            case OAK_LOG:
+                return toolType == Material.DIAMOND_AXE;
+            default:
+                return false;
+        }
     }
 }
