08c3278def311ef5750b4621703e1f902abd7abd
ShadowStrikeTwitch <leonsahl@t-online.de>
2025-04-01 14:15:49 +0200

    Git innit

---
+++ src/main/java/de/vynora/blockBreak/BlockBreak.java
@@ -0,0 +1,17 @@
+package de.vynora.blockBreak;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BlockBreak extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
